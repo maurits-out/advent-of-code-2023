441855f4f3c7309ccc21dcd74084e87fb93aaf2d
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2023-12-25 12:37:17 +0100

    Day 25

---
+++ src/main/java/nl/mout/aoc2023/day25/Snowverload.java
@@ -0,0 +1,41 @@
+package nl.mout.aoc2023.day25;
+
+import org.jgrapht.alg.StoerWagnerMinimumCut;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import java.util.StringTokenizer;
+
+import static java.util.Collections.list;
+import static nl.mout.aoc2023.support.InputLoader.loadInput;
+
+public class Snowverload {
+
+    private final SimpleGraph<String, DefaultEdge> graph;
+
+    public Snowverload(String input) {
+        graph = new SimpleGraph<>(DefaultEdge.class);
+        input.lines().forEach(line -> {
+            var tokenizer = new StringTokenizer(line);
+            var from = tokenizer.nextToken().substring(0, 3);
+            graph.addVertex(from);
+            list(tokenizer).forEach(token -> {
+                var to = (String) token;
+                graph.addVertex(to);
+                graph.addEdge(from, to);
+            });
+        });
+    }
+
+    public int part1() {
+        var stoerWagner = new StoerWagnerMinimumCut<>(graph);
+        var sizeMinCut = stoerWagner.minCut().size();
+        return sizeMinCut * (graph.vertexSet().size() - sizeMinCut);
+    }
+
+    public static void main(String[] args) {
+        var input = loadInput("day25-input.txt");
+        var snowverload = new Snowverload(input);
+        System.out.println("Answer: " + snowverload.part1());
+    }
+}
