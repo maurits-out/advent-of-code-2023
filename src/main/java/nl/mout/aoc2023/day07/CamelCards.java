3a17a6bfafddc8f5076f72891354b20b6095e4c7
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2023-12-28 16:46:27 +0100

    Refactoring.

--- src/main/java/nl/mout/aoc2023/day07/CamelCards.java
+++ src/main/java/nl/mout/aoc2023/day07/CamelCards.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.Integer.parseInt;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.*;
 import static java.util.stream.IntStream.range;
@@ -12,20 +13,58 @@ import static nl.mout.aoc2023.support.InputLoader.loadInput;
 
 public class CamelCards {
 
-    final List<Hand> hands;
+    private final List<Hand> hands;
 
-    CamelCards(String input) {
+    public CamelCards(String input) {
         hands = input.lines()
                 .map(this::parseLine)
                 .toList();
     }
 
-    Hand parseLine(String line) {
-        var components = line.split(" ");
-        return new Hand(components[0], Integer.parseInt(components[1]), getHandTypeWithoutJoker(components[0]), getHandTypeWithJoker(components[0]));
+    public int part1() {
+        Comparator<Hand> byType = Comparator.comparing(hand -> hand.typePart1);
+        var byStrongestCard = new StrongestCardComparator('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A');
+        return countTotalWinnings(byType, byStrongestCard);
     }
 
-    HandType getHandTypeWithoutJoker(String cards) {
+    public int part2() {
+        Comparator<Hand> byType = Comparator.comparing(hand -> hand.typePart2);
+        var byStrongestCard = new StrongestCardComparator('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A');
+        var sorted = hands.stream()
+                .sorted(byType.thenComparing(byStrongestCard))
+                .toArray(Hand[]::new);
+        return range(0, sorted.length)
+                .map(index -> (index + 1) * sorted[index].bid)
+                .sum();
+    }
+
+
+    private int countTotalWinnings(Comparator<Hand> byType, StrongestCardComparator byStrongestCard) {
+        var sorted = hands.stream()
+                .sorted(byType.thenComparing(byStrongestCard))
+                .toArray(Hand[]::new);
+        return range(0, sorted.length)
+                .map(index -> (index + 1) * sorted[index].bid)
+                .sum();
+    }
+
+    private record Hand(String cards, int bid, HandType typePart1, HandType typePart2) {
+    }
+
+    enum HandType {
+        HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND
+    }
+
+    private Hand parseLine(String line) {
+        var components = line.split(" ");
+        return new Hand(components[0],
+                parseInt(components[1]),
+                getHandTypeWithoutJoker(components[0]),
+                getHandTypeWithJoker(components[0])
+        );
+    }
+
+    private HandType getHandTypeWithoutJoker(String cards) {
         var counts = cards.chars()
                 .boxed()
                 .collect(groupingBy(ch -> (char) ch.intValue(), counting()))
@@ -53,41 +92,40 @@ public class CamelCards {
         return type;
     }
 
-
-    HandType getHandTypeWithJoker(String cards) {
-        var counts = cards.chars()
+    private HandType getHandTypeWithJoker(String cards) {
+        var countsExcludingJoker = cards.chars()
                 .filter(ch -> ch != 'J')
                 .boxed()
                 .collect(groupingBy(ch -> (char) ch.intValue(), counting()))
                 .values().stream()
                 .collect(groupingBy(Long::intValue, counting()));
-        var jokerCount = (int) cards.chars().filter(ch -> ch == 'J').count();
+        var jokerCount = cards.chars().filter(ch -> ch == 'J').count();
 
         HandType type;
         if (jokerCount >= 4) {
             type = FIVE_OF_A_KIND;
         } else if (jokerCount == 3) {
-            if (counts.getOrDefault(2, 0L) == 1) {
+            if (countsExcludingJoker.getOrDefault(2, 0L) == 1) {
                 type = FIVE_OF_A_KIND;
             } else {
                 type = FOUR_OF_A_KIND;
             }
         } else if (jokerCount == 2) {
-            if (counts.getOrDefault(3, 0L) == 1) {
+            if (countsExcludingJoker.getOrDefault(3, 0L) == 1) {
                 type = FIVE_OF_A_KIND;
-            } else if (counts.getOrDefault(2, 0L) == 1) {
+            } else if (countsExcludingJoker.getOrDefault(2, 0L) == 1) {
                 type = FOUR_OF_A_KIND;
             } else {
                 type = THREE_OF_A_KIND;
             }
         } else if (jokerCount == 1) {
-            if (counts.getOrDefault(4, 0L) == 1) {
+            if (countsExcludingJoker.getOrDefault(4, 0L) == 1) {
                 type = FIVE_OF_A_KIND;
-            } else if (counts.getOrDefault(3, 0L) == 1) {
+            } else if (countsExcludingJoker.getOrDefault(3, 0L) == 1) {
                 type = FOUR_OF_A_KIND;
-            } else if (counts.getOrDefault(2, 0L) == 2) {
+            } else if (countsExcludingJoker.getOrDefault(2, 0L) == 2) {
                 type = FULL_HOUSE;
-            } else if (counts.getOrDefault(2, 0L) == 1) {
+            } else if (countsExcludingJoker.getOrDefault(2, 0L) == 1) {
                 type = THREE_OF_A_KIND;
             } else {
                 type = ONE_PAIR;
@@ -98,14 +136,14 @@ public class CamelCards {
         return type;
     }
 
-    static class StrongestCardComparator implements Comparator<Hand> {
+    private static class StrongestCardComparator implements Comparator<Hand> {
 
         private final Map<Character, Integer> indexByLabel;
 
         StrongestCardComparator(char... labels) {
-            this.indexByLabel = range(0, labels.length).boxed().collect(toUnmodifiableMap(
-                idx -> labels[idx], identity()
-            ));
+            this.indexByLabel = range(0, labels.length)
+                    .boxed()
+                    .collect(toMap(idx -> labels[idx], identity()));
         }
 
         @Override
@@ -119,39 +157,10 @@ public class CamelCards {
         }
     }
 
-    record Hand(String cards, int bid, HandType typePart1, HandType typePart2) {
-    }
-
-    enum HandType {
-        HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND
-    }
-
-    int part1() {
-        var byStrongestCard = new StrongestCardComparator('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A');
-
-        var sorted = hands.stream()
-                .sorted(Comparator.<Hand, HandType>comparing(hand -> hand.typePart1).thenComparing(byStrongestCard))
-                .toArray(Hand[]::new);
-        return range(0, sorted.length)
-                .map(index -> (index + 1) * sorted[index].bid)
-                .sum();
-    }
-
-    int part2() {
-        var byStrongestCard = new StrongestCardComparator('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A');
-
-        var sorted = hands.stream()
-                .sorted(Comparator.<Hand, HandType>comparing(hand -> hand.typePart2).thenComparing(byStrongestCard))
-                .toArray(Hand[]::new);
-        return range(0, sorted.length)
-                .map(index -> (index + 1) * sorted[index].bid)
-                .sum();
-    }
-
     public static void main(String[] args) {
         var input = loadInput("day07-input.txt");
         var camelCards = new CamelCards(input);
-        System.out.println("Part 1: " + camelCards.part1());
-        System.out.println("Part 2: " + camelCards.part2());
+        System.out.printf("Part 1: %d\n", camelCards.part1());
+        System.out.printf("Part 2: %d\n", camelCards.part2());
     }
 }
