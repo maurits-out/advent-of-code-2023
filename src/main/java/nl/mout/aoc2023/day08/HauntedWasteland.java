57a8fedaba368fbd65a10a24e716c4938c98a5a0
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2023-12-28 16:57:05 +0100

    Refactoring.

--- src/main/java/nl/mout/aoc2023/day08/HauntedWasteland.java
+++ src/main/java/nl/mout/aoc2023/day08/HauntedWasteland.java
@@ -1,5 +1,7 @@
 package nl.mout.aoc2023.day08;
 
+import org.apache.commons.math3.util.ArithmeticUtils;
+
 import java.util.Map;
 import java.util.function.Predicate;
 
@@ -8,10 +10,10 @@ import static nl.mout.aoc2023.support.InputLoader.loadInput;
 
 public class HauntedWasteland {
 
-    final char[] instructions;
-    final Map<String, String[]> nodes;
+    private final char[] instructions;
+    private final Map<String, String[]> nodes;
 
-    HauntedWasteland(String input) {
+    public HauntedWasteland(String input) {
         instructions = input.lines().findFirst().orElseThrow().toCharArray();
         nodes = input.lines().skip(2).collect(toMap(
                 line -> line.substring(0, 3),
@@ -19,43 +21,33 @@ public class HauntedWasteland {
         ));
     }
 
-    int countSteps(String start, Predicate<String> stop) {
+    public int part1() {
+        return countSteps("AAA", node -> node.equals("ZZZ"));
+    }
+
+    public long part2() {
+        return nodes.keySet().stream()
+                .filter(n -> n.endsWith("A"))
+                .map(start -> Long.valueOf(countSteps(start, node -> node.endsWith("Z"))))
+                .reduce(ArithmeticUtils::lcm).orElseThrow();
+    }
+
+    private int countSteps(String start, Predicate<String> stop) {
         int ip = 0, steps = 0;
         var current = start;
         while (!stop.test(current)) {
-            current = (instructions[ip] == 'L') ? nodes.get(current)[0] : nodes.get(current)[1];
+            var elements = nodes.get(current);
+            current = (instructions[ip] == 'L') ? elements[0] : elements[1];
             ip = (ip < instructions.length - 1) ? ip + 1 : 0;
             steps++;
         }
         return steps;
     }
 
-    long lcm(long a, long b) {
-        return (a * b) / gcd(a, b);
-    }
-
-    long gcd(long a, long b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
-
-    int part1() {
-        return countSteps("AAA", node -> node.equals("ZZZ"));
-    }
-
-    long part2() {
-        return nodes.keySet().stream()
-                .filter(n -> n.endsWith("A"))
-                .map(start -> Long.valueOf(countSteps(start, node -> node.endsWith("Z"))))
-                .reduce(this::lcm).orElseThrow();
-    }
-
     public static void main(String[] args) {
         var input = loadInput("day08-input.txt");
         var hauntedWasteland = new HauntedWasteland(input);
-        System.out.println("Part 1: " + hauntedWasteland.part1());
-        System.out.println("Part 2: " + hauntedWasteland.part2());
+        System.out.printf("Part 1: %d\n", hauntedWasteland.part1());
+        System.out.printf("Part 2: %d\n", hauntedWasteland.part2());
     }
 }
